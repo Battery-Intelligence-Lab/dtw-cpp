d13dc40b29fbf2d1dbc11443e5fdcc0f339faeb9
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2022-12-05 00:34:17 +0000

    std::filesystem::path operator+ is removed.

--- dtwc/settings.hpp
+++ dtwc/settings.hpp
@@ -11,19 +11,13 @@
 using data_t = double;      // Data type double or float.
 using ind_t = unsigned int; // Data type for indices.
 
-inline std::filesystem::path operator+(const std::filesystem::path &lhs, const std::string &rhs)
-{ //!< To make path type compatible with strings.
-  const std::filesystem::path temp{ rhs };
-  return lhs / temp;
-}
-
 namespace settings {
 
 namespace fs = std::filesystem;
 
 const static fs::path root_folder = "../../";
 
-const auto resultsPath = root_folder + "results/";
+const auto resultsPath = root_folder / "results/";
 constexpr bool isParallel = true;
 
 constexpr int numMaxParallelWorkers = 1024; // Change accordingly more cores than your computer has. It is limited to the maximum physical cores.
@@ -32,6 +26,6 @@ constexpr bool writeAsFileNames = true;
 constexpr bool isDebug = false;
 
 
-const auto path = root_folder + "data/dummy";
+const auto dataPath = root_folder / "data";
 constexpr int band = 0; // Size of band to use (if no band put 0)
 } // namespace settings
