018d7c5671a31dc4b0365cb99f263ba48724e7d8
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2022-12-08 11:58:43 +0000

    some more tests

--- matlab/test_not_turtley.m
+++ matlab/test_not_turtley.m
@@ -9,8 +9,35 @@ distanceMat = readmatrix('AllGestureWiimoteX_TEST_distanceMatrix.csv');
 assert(size(distanceMat,1) == size(distanceMat,2)); % See if it is square
 
 tic; 
-sol = findBestMedoids(distanceMat, Nc, 2);
+sol_mip = findBestMedoids(distanceMat, Nc, 2);
 
 toc
 
 fprintf('Cost: %4.6f\n',sol.cost);
+
+%%
+
+sol_turtle = findBestMedoids(distanceMat, Nc, 2, true);
+
+%%
+
+figure; spy(sol_mip.w)
+
+%%
+
+sol_penalized_isCluster = findBestMedoids(distanceMat, Nc, 2, true);
+
+%%
+
+sol_mip2 = findBestMedoids(distanceMat, Nc, 2); %% Lower gap! 
+
+%%
+
+sol_turtle2 = findBestMedoids(distanceMat, Nc, 2, true);
+
+%%
+
+sol_turtle_Mosek = findBestMedoids(distanceMat, Nc, 2, true);
+
+%%
+
