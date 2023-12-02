ba56134af3ef84b3b8bdfdb6d750a03f839ae703
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2023-12-02 03:28:16 +0000

    removing utility.hpp

--- dtwc/utility.hpp
+++ dtwc/utility.hpp
@@ -13,20 +13,3 @@
 #include "types/types.hpp"
 #include "fileOperations.hpp"
 #include "parallelisation.hpp"
-
-#include <iostream>
-#include <vector>
-#include <array>
-
-#include <numeric>
-#include <fstream>
-#include <limits>
-#include <ctime>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <random>
-
-namespace dtwc {
-
-}; // namespace dtwc
