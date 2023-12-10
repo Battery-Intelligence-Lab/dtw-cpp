52046ba328699eff8382bb0ae629e0ee26baaa37
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2023-12-10 19:00:23 +0000

    OpenMP is added.

--- dtwc/parallelisation.hpp
+++ dtwc/parallelisation.hpp
@@ -12,23 +12,37 @@
 #include "settings.hpp"
 #include "types/Range.hpp"
 
-#include <execution>
-#include <algorithm>
+// #include <execution>
+// #include <algorithm>
 #include <cstddef>
+#include "omp.h"
 
 
 namespace dtwc {
 
-namespace ex = std::execution;
+// namespace ex = std::execution;
+
+// template <typename Tfun>
+// void run_std(Tfun &task_indv, size_t i_end, bool isParallel = settings::isParallel)
+// {
+//   auto range = Range(i_end);
+//   if (isParallel)
+//     std::for_each(ex::par_unseq, range.begin(), range.end(), task_indv);
+//   else
+//     std::for_each(ex::seq, range.begin(), range.end(), task_indv);
+// }
 
 template <typename Tfun>
-void run_std(Tfun &task_indv, size_t i_end, bool isParallel = settings::isParallel)
+void run_openmp(Tfun &task_indv, size_t i_end, bool isParallel = settings::isParallel)
 {
-  auto range = Range(i_end);
-  if (isParallel)
-    std::for_each(ex::par_unseq, range.begin(), range.end(), task_indv);
-  else
-    std::for_each(ex::seq, range.begin(), range.end(), task_indv);
+  if (isParallel) {
+#pragma omp parallel for schedule(dynamic) // As some take less time static scheduling is 2x slower.
+    for (int i = 0; i < i_end; i++) {
+      task_indv(i);
+    }
+  } else
+    for (int i = 0; i < i_end; i++)
+      task_indv(i);
 }
 
 
@@ -36,6 +50,6 @@ template <typename Tfun>
 void run(Tfun &task_indv, size_t i_end, size_t numMaxParallelWorkers = settings::numMaxParallelWorkers)
 {
   std::cout << "Standard algorithms parallelisation is being used." << std::endl;
-  run_std(task_indv, i_end, numMaxParallelWorkers > 1);
+  run_openmp(task_indv, i_end, numMaxParallelWorkers > 1);
 }
 } // namespace dtwc
