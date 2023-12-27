d3c0d825d5a4866cb4072fe04353779e7b1c36f0
Volkan Kumtepeli <volkan.kumtepeli@gmail.com>
2023-12-27 00:54:36 +0000

    load_batch_file test added.

--- tests/unit/unit_test_fileOperations.cpp
+++ tests/unit/unit_test_fileOperations.cpp
@@ -16,6 +16,10 @@
 
 #include <string>
 #include <sstream>
+#include <fstream>
+#include <random>
+#include <algorithm>
+#include <iterator>
 
 using Catch::Matchers::WithinAbs;
 
@@ -97,4 +101,80 @@ TEST_CASE("Write and Read Empty Matrix", "[fileOperations]")
   REQUIRE(readMat.cols() == 0);
 
   fs::remove(tempFilePath); // Clean up the test file
+}
+
+TEST_CASE("Load batch file", "[fileOperations]")
+{
+  std::string tempFileName = "test_matrix";
+
+  // Generate data:
+  const int N_data = GENERATE(1, 2, 10, 1000); // Size of the outer vector
+  const int L_data = GENERATE(1, 2, 10, 1000); // Maximum size of the inner vectors
+
+  std::vector<std::vector<double>> random_data;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, L_data);
+
+
+  for (int i = 0; i < N_data; ++i) {
+    int innerSize = dis(gen); // Random size for the inner vector
+    std::vector<double> innerVector;
+
+    for (int j = 0; j < innerSize; ++j)
+      innerVector.push_back(dis(gen)); // Generate random number
+
+    random_data.push_back(std::move(innerVector));
+  }
+
+  // write the files
+  {
+    std::ofstream out_csv(tempFileName + ".csv", std::ios_base::out);
+    std::ofstream out_tsv(tempFileName + ".tsv", std::ios_base::out);
+
+
+    for (const auto &innerVector : random_data) {
+      for (size_t i = 0; i < innerVector.size(); i++) {
+        if (i != 0) {
+          out_csv << ',';
+          out_tsv << '\t';
+        }
+
+        out_csv << innerVector[i];
+        out_tsv << innerVector[i];
+      }
+
+      out_csv << '\n';
+      out_tsv << '\n';
+    }
+  } // Auto close at the end thanks to the destructor.
+
+
+  // ----- now testing -----
+  SECTION("csv batch load")
+  {
+    fs::path pth = tempFileName + ".csv";
+    int start_row{ 0 }, start_col{}, delimiter{ ',' };
+    auto [p_vec, p_names] = load_batch_file<double>(pth, N_data, false, start_row, start_col, delimiter);
+
+    for (size_t i{}; i < N_data; i++)
+      REQUIRE(p_names[i] == std::to_string(i + 1));
+
+    REQUIRE(p_vec == random_data);
+  }
+
+  SECTION("tsv batch load")
+  {
+    fs::path pth = tempFileName + ".tsv";
+    int start_row{ 0 }, start_col{}, delimiter{ '\t' };
+    auto [p_vec, p_names] = load_batch_file<double>(pth, N_data, false, start_row, start_col, delimiter);
+
+    for (size_t i{}; i < N_data; i++)
+      REQUIRE(p_names[i] == std::to_string(i + 1));
+
+    REQUIRE(p_vec == random_data);
+  }
+
+  fs::remove(tempFileName + ".csv"); // Clean up the test files
+  fs::remove(tempFileName + ".tsv"); // Clean up the test files
 }
